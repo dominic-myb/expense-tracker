659ffd92746769546a7058d945ce9a6874f39549
dominic-myb <desguerra246@gmail.com>
2024-01-20 14:35:42 +0800

    Added BS & Set Up PHP

--- index.php
+++ index.php
@@ -1,24 +1,54 @@
 <?php include("includes/connection.php"); ?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Expense Tracker</title>
-</head>
+<?php 
+    $title = "Expense Tracker";
+    include("includes/html.head.php"); 
+?>
 <body>
     <div>
         <?php 
-            $sql = "SELECT * FROM savings_tbl";
+            $sql = "SELECT * FROM income_tbl";
             $result = mysqli_query($conn, $sql);
             $balance = 0;
             if($result->num_rows > 0){
-                while($rows = mysqli_fetch_assoc($result)){
-                    $balance += $rows['income'];
-                }
-            }
+                while($row = mysqli_fetch_assoc($result)){
+                    $balance += $row['income'];
         ?>
         <h3>Balance: <?php echo $balance; ?></h3>
+        <table class="table table-hover table-borderless align-middle">
+            <thead>
+                <tr class="table-primary">
+                    <th scope="col">No.</th>
+                    <th scope="col">Income</th>
+                    <th scope="col">Date</th>
+                    <th scope="col">Description</th>
+                    <th scope="col">Actions</th>
+                </tr>
+            </thead>
+            <tbody class="table-group-divider">
+                <tr>
+                    <td><?php echo $row['id'];?></td>
+                    <td><?php echo $row['income'];?></td>
+                    <td><?php echo $row['date'];?></td>
+                    <td><?php echo $row['description'];?></td>
+                    <td>
+                        <button type="button" class="btn btn-primary">Update</button>
+                        <button type="button" class="btn btn-danger">Delete</button>
+                    </td>
+                </tr>
+                <?php
+                        }
+                    }else{
+                        ?>
+                        <tr>
+                            <td colspan="5" style="text-align:center;">No Records</td>
+                        </tr>
+                        <?php
+                    }
+                ?>
+            </tbody>
+        </table>
     </div>
 </body>
 </html>
