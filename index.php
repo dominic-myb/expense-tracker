637e2d12ae0277e8f2d63f3ae4253f5fe3f32422
dominic-myb <desguerra246@gmail.com>
2024-01-25 16:23:38 +0800

    Added Modal

--- index.php
+++ index.php
@@ -15,7 +15,12 @@
                 while($row = mysqli_fetch_assoc($result)){
                     $balance += $row['income'];
         ?>
-        <h3>Balance: <?php echo $balance; ?></h3>
+        <div class="top-header">
+            <h3>Balance: <?php echo $balance; ?></h3>
+            <input class="btn btn-success" type="button" name="income" id="income" value="Income" data-bs-toggle="modal" data-bs-target="#income-modal">
+            <input class="btn btn-danger" type="button" name="expense" id="expense" value="Expense" data-bs-toggle="modal" data-bs-target="#expense-modal">
+        </div>
+
         <table class="table table-hover table-borderless align-middle">
             <thead>
                 <tr class="table-primary">
@@ -50,5 +55,46 @@
             </tbody>
         </table>
     </div>
+    <!-- Income Section Modal -->
+    <div class="modal fade" id="income-modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Add Income</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <input class="form-control" type="text" placeholder="Amount" aria-label="default input example"><br>
+                    <textarea class="form-control" id="exampleFormControlTextarea1" placeholder="Description" rows="3"></textarea>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End of Income Modal -->
+    <!-- Start of Expense Modal -->
+    <div class="modal fade" id="expense-modal" tabindex="-1" role="dialog" aria-labelledby="expense-label" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="expense-label">Add Expense</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <input class="form-control" type="text" placeholder="Amount" aria-label="default input example"><br>
+                    <textarea class="form-control" id="exampleFormControlTextarea1" placeholder="Description" rows="3"></textarea>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End of Expense Modal -->
+
 </body>
 </html>
