172b01abcab537814fc6b4cb4855283a3ca22456
dominic-myb <desguerra246@gmail.com>
2024-02-07 23:03:54 +0800

    set up the links and source

---
+++ app/includes/html/record.foot.php
@@ -0,0 +1,3 @@
+<script src="assets/js/vendor/jquery-3.7.1.min.js"></script>
+<script src="assets/css/vendor/bootstrap-5.2.3-dist/js/bootstrap.min.js"></script>
+<script src="assets/js/record.script.js"></script>
