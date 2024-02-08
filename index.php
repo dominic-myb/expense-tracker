519667c36e09e81daa9fba511ed80eb7b86401f7
dominic-myb <desguerra246@gmail.com>
2024-02-08 23:23:34 +0800

    add an updating balance

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
-<!-- TODO List:
-- Add an update 
-- Add delete
--->
-<?php include("includes/connection.php"); ?>
+<?php include("app/includes/components/connection.php");
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <?php
@@ -14,29 +12,33 @@ include("app/includes/html/html.head.php");
     <div>
         <?php
         $sql = "SELECT * FROM income_tbl";
-        $result = mysqli_query($conn, $sql);
+        $result = $conn->query($sql);
         $balance = 0;
-        if ($result->num_rows > 0) {
-            while ($row = mysqli_fetch_assoc($result)) {
-                $balance += $row['income'];
+        $balanceQuery = "SELECT SUM(income) as sum_income FROM income_tbl";
+        $balance = $conn->query($balanceQuery);
+        $balaceData = $balance->fetch_assoc();
+        $balance = $balaceData["sum_income"];
         ?>
-                <div class="top-header">
-                    <h3>Balance: <?php echo $balance; ?></h3>
-                    <input class="btn btn-success" type="button" name="income" id="income" value="Income" data-bs-toggle="modal" data-bs-target="#income-modal">
-                    <input class="btn btn-danger" type="button" name="expense" id="expense" value="Expense" data-bs-toggle="modal" data-bs-target="#expense-modal">
-                </div>
-
-                <table class="table table-hover table-borderless align-middle">
-                    <thead>
-                        <tr class="table-primary">
-                            <th scope="col">No.</th>
-                            <th scope="col">Income</th>
-                            <th scope="col">Date</th>
-                            <th scope="col">Description</th>
-                            <th scope="col">Actions</th>
-                        </tr>
-                    </thead>
-                    <tbody class="table-group-divider">
+        <div class="top-header">
+            <h3>Balance: <?php echo $balance; ?></h3>
+            <input class="btn btn-success" type="button" name="income" id="income" value="Income" data-bs-toggle="modal" data-bs-target="#income-modal">
+            <input class="btn btn-danger" type="button" name="expense" id="expense" value="Expense" data-bs-toggle="modal" data-bs-target="#expense-modal">
+        </div>
+        <table class="table table-hover table-borderless align-middle">
+            <thead>
+                <tr class="table-primary">
+                    <th scope="col">No.</th>
+                    <th scope="col">Income</th>
+                    <th scope="col">Date</th>
+                    <th scope="col">Description</th>
+                    <th scope="col">Actions</th>
+                </tr>
+            </thead>
+            <tbody class="table-group-divider">
+                <?php
+                if ($result->num_rows > 0) {
+                    while ($row = mysqli_fetch_assoc($result)) {
+                ?>
                         <tr>
                             <td><?php echo $row['id']; ?></td>
                             <td><?php echo $row['income']; ?></td>
@@ -48,34 +50,36 @@ include("app/includes/html/html.head.php");
                             </td>
                         </tr>
                     <?php
-                }
-            } else {
+                    }
+                } else {
                     ?>
                     <tr>
                         <td colspan="5" style="text-align:center;">No Records</td>
                     </tr>
                 <?php
-            }
+                }
                 ?>
-                    </tbody>
-                </table>
+            </tbody>
+        </table>
     </div>
     <!-- Income Section Modal -->
-    <div class="modal fade" id="income-modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal fade" id="income-modal" tabindex="-1" role="dialog" aria-labelledby="income-label" aria-hidden="true">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Add Income</h5>
+                    <h5 class="modal-title" id="income-label">Add Income</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
-                <div class="modal-body">
-                    <input class="form-control" type="text" placeholder="Amount" aria-label="default input example"><br>
-                    <textarea class="form-control" id="exampleFormControlTextarea1" placeholder="Description" rows="3"></textarea>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="submit" class="btn btn-primary">Submit</button>
-                </div>
+                <form action="income.php" method="post">
+                    <div class="modal-body">
+                        <input class="form-control" name="amount" type="text" placeholder="Amount" aria-label="default input example"><br>
+                        <textarea class="form-control" name="desc" id="text-area" placeholder="Description" rows="3"></textarea>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
