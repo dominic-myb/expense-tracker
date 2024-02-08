f250caf1d46095ab227891704e4b01de81ba1d56
dominic-myb <desguerra246@gmail.com>
2024-02-08 23:23:06 +0800

    added income php script

--- income.php
+++ income.php
@@ -1,14 +1,20 @@
-<!DOCTYPE html>
-<html lang="en">
 <?php
-$pageTitle = "Expense";
-$style = "record.style.css";
-include("app/includes/html/record.head.php");
-?>
-
-<body>
-
-    <?php include("app/includes/html/record.foot.php"); ?>
-</body>
-
-</html>
+include("app/includes/components/connection.php");
+if ($_SERVER["REQUEST_METHOD"] == "POST"){
+    $amount = $_POST['amount'];
+    $desc = $_POST['desc'];
+    $date = 1;
+    $stmt = $conn->prepare('INSERT INTO income_tbl (income, date, description)VALUES(?,?,?)');
+    $stmt->bind_param('iss', $amount, $date, $desc);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    if($stmt->affected_rows > 0){
+        $message = 'Recorded Successfully';
+    }else{
+        $message = 'An error occured';
+    }
+    echo "<script>alert($message)
+        window.location = 'index.php'</script>";
+    $stmt->close();
+    exit;
+}
