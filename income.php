c988bd0588184339d2719a8d546e9d1e1d8a9bbe
dominic-myb <desguerra246@gmail.com>
2024-02-09 11:07:36 +0800

    added date and time

--- income.php
+++ income.php
@@ -3,9 +3,8 @@ include("app/includes/components/connection.php");
 if ($_SERVER["REQUEST_METHOD"] == "POST"){
     $amount = $_POST['amount'];
     $desc = $_POST['desc'];
-    $date = 1;
-    $stmt = $conn->prepare('INSERT INTO income_tbl (income, date, description)VALUES(?,?,?)');
-    $stmt->bind_param('iss', $amount, $date, $desc);
+    $stmt = $conn->prepare('INSERT INTO income_tbl (income, date, description)VALUES(?,CURDATE(),?)');
+    $stmt->bind_param('ds', $amount, $desc);
     $stmt->execute();
     $result = $stmt->get_result();
     if($stmt->affected_rows > 0){
@@ -13,7 +12,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
     }else{
         $message = 'An error occured';
     }
-    echo "<script>alert($message)
+    echo "<script>alert('$message')
         window.location = 'index.php'</script>";
     $stmt->close();
     exit;
